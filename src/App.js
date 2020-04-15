89e6e83c6e67d0d77401a5664f468c5b8480596e
Alice <alice828@mail.ubc.ca>
2020-04-15 14:42:58 -0700

    Implement Search Page using NASA API (#1)
    * create search page using NASA API
    
    * route to the search page
    
    Co-authored-by: Alice Kim <alice.kim@concur.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Header } from "./components/Header"
 import { MapPage } from "./pages/Maps"
 import { MainPage } from "./pages/Main"
 import { CardPage } from "./pages/Cards"
+import SearchPage from "./pages/Search"
 import './App.css';
 
 function App() {
@@ -17,6 +18,9 @@ function App() {
           </Route>
           <Route path={"/cards"}>
             <CardPage />
+          </Route>
+          <Route path={"/nasa"}>
+            <SearchPage />
           </Route>
           <Route path={"/"}>
             <MainPage />
